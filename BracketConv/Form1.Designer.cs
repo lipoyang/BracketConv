cde02d3b04b20309863c7d2cc49abaaa94bc4a46
lipoyang <lipoyang1453@gmail.com>
2022-09-28 10:46:30 +0900

    add files

---
+++ BracketConv/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace BracketConv
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textInputFolder = new System.Windows.Forms.TextBox();
+            this.textOutputFolder = new System.Windows.Forms.TextBox();
+            this.btnInputFolder = new System.Windows.Forms.Button();
+            this.btnOutputFolder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textFilter = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textVarNames = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.checkSubFolder = new System.Windows.Forms.CheckBox();
+            this.textStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textInputFolder
+            // 
+            this.textInputFolder.Location = new System.Drawing.Point(12, 24);
+            this.textInputFolder.Name = "textInputFolder";
+            this.textInputFolder.Size = new System.Drawing.Size(570, 19);
+            this.textInputFolder.TabIndex = 0;
+            // 
+            // textOutputFolder
+            // 
+            this.textOutputFolder.Location = new System.Drawing.Point(12, 113);
+            this.textOutputFolder.Name = "textOutputFolder";
+            this.textOutputFolder.Size = new System.Drawing.Size(570, 19);
+            this.textOutputFolder.TabIndex = 1;
+            // 
+            // btnInputFolder
+            // 
+            this.btnInputFolder.Location = new System.Drawing.Point(501, 49);
+            this.btnInputFolder.Name = "btnInputFolder";
+            this.btnInputFolder.Size = new System.Drawing.Size(81, 26);
+            this.btnInputFolder.TabIndex = 2;
+            this.btnInputFolder.Text = "参照";
+            this.btnInputFolder.UseVisualStyleBackColor = true;
+            this.btnInputFolder.Click += new System.EventHandler(this.btnInputFolder_Click);
+            // 
+            // btnOutputFolder
+            // 
+            this.btnOutputFolder.Location = new System.Drawing.Point(501, 138);
+            this.btnOutputFolder.Name = "btnOutputFolder";
+            this.btnOutputFolder.Size = new System.Drawing.Size(81, 26);
+            this.btnOutputFolder.TabIndex = 3;
+            this.btnOutputFolder.Text = "参照";
+            this.btnOutputFolder.UseVisualStyleBackColor = true;
+            this.btnOutputFolder.Click += new System.EventHandler(this.btnOutputFolder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "入力フォルダ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "出力フォルダ";
+            // 
+            // textFilter
+            // 
+            this.textFilter.Location = new System.Drawing.Point(56, 60);
+            this.textFilter.Name = "textFilter";
+            this.textFilter.Size = new System.Drawing.Size(115, 19);
+            this.textFilter.TabIndex = 6;
+            this.textFilter.Text = "*.cs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "フィルタ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "括弧の前の変数名";
+            // 
+            // textVarNames
+            // 
+            this.textVarNames.Location = new System.Drawing.Point(115, 183);
+            this.textVarNames.Multiline = true;
+            this.textVarNames.Name = "textVarNames";
+            this.textVarNames.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textVarNames.Size = new System.Drawing.Size(352, 236);
+            this.textVarNames.TabIndex = 9;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(501, 393);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(81, 26);
+            this.btnConvert.TabIndex = 10;
+            this.btnConvert.Text = "変換";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // checkSubFolder
+            // 
+            this.checkSubFolder.AutoSize = true;
+            this.checkSubFolder.Location = new System.Drawing.Point(194, 63);
+            this.checkSubFolder.Name = "checkSubFolder";
+            this.checkSubFolder.Size = new System.Drawing.Size(111, 16);
+            this.checkSubFolder.TabIndex = 11;
+            this.checkSubFolder.Text = "サブフォルダも検索";
+            this.checkSubFolder.UseVisualStyleBackColor = true;
+            // 
+            // textStatus
+            // 
+            this.textStatus.AutoSize = true;
+            this.textStatus.Location = new System.Drawing.Point(14, 431);
+            this.textStatus.Name = "textStatus";
+            this.textStatus.Size = new System.Drawing.Size(50, 12);
+            this.textStatus.TabIndex = 12;
+            this.textStatus.Text = "ステータス";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 455);
+            this.Controls.Add(this.textStatus);
+            this.Controls.Add(this.checkSubFolder);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.textVarNames);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textFilter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOutputFolder);
+            this.Controls.Add(this.btnInputFolder);
+            this.Controls.Add(this.textOutputFolder);
+            this.Controls.Add(this.textInputFolder);
+            this.Name = "FormMain";
+            this.Text = "括弧置換ツール ()→[]";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textInputFolder;
+        private System.Windows.Forms.TextBox textOutputFolder;
+        private System.Windows.Forms.Button btnInputFolder;
+        private System.Windows.Forms.Button btnOutputFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textFilter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textVarNames;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.CheckBox checkSubFolder;
+        private System.Windows.Forms.Label textStatus;
+    }
+}
+
---
+++ BracketConv/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; // FileInfo
+using System.Text.RegularExpressions; // 正規表現
+using IniFileSharp; // INIファイル
+
+namespace BracketConv
+{
+    public partial class FormMain : Form
+    {
+        private int ErrorNum = 0; // エラーの数
+        private IniFile iniFile = new IniFile(); // INIファイル
+
+        public FormMain()
+        {
+            InitializeComponent();
+            
+            textStatus.Text = "";
+
+            // INIファイルの設定読み込み
+            textInputFolder.Text   = iniFile.ReadString( "PATH",   "INPUT", "");
+            textOutputFolder.Text  = iniFile.ReadString( "PATH",   "OUTPUT", "");
+            textFilter.Text        = iniFile.ReadString( "OPTION", "FILTER", "*.cs");
+            checkSubFolder.Checked = iniFile.ReadBoolean("OPTION", "SUBFOLDER", false);
+        }
+
+        // 終了時にINIファイルに設定保存
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            iniFile.WriteString ("PATH",   "INPUT",     textInputFolder.Text);
+            iniFile.WriteString ("PATH",   "OUTPUT",    textOutputFolder.Text);
+            iniFile.WriteString ("OPTION", "FILTER",    textFilter.Text);
+            iniFile.WriteBoolean("OPTION", "SUBFOLDER", checkSubFolder.Checked);
+        }
+
+        // 入力フォルダの選択
+        private void btnInputFolder_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            fbd.Description = "入力フォルダを指定してください。";
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            fbd.ShowNewFolderButton = false;
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                textInputFolder.Text = fbd.SelectedPath;
+            }
+        }
+
+        // 出力フォルダの選択
+        private void btnOutputFolder_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            fbd.Description = "出力フォルダを指定してください。";
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+            fbd.ShowNewFolderButton = true;
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                textOutputFolder.Text = fbd.SelectedPath;
+            }
+        }
+        
+        // 変換処理
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            textStatus.Text = "処理を開始します。";
+            ErrorNum = 0;
+
+            // ファイル一覧を取得
+            var di = new System.IO.DirectoryInfo(textInputFolder.Text);
+            var option = checkSubFolder.Checked ? // サブフォルダも？
+                SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            FileInfo[] files = di.GetFiles(textFilter.Text, option);
+
+            // 変数名一覧を取得
+            string[] varNames = textVarNames.Text.Split(
+                new[] { Environment.NewLine }, StringSplitOptions.None);
+            
+            // 各々のファイルについて
+            foreach(var file in files)
+            {
+                textStatus.Text = "処理中: " + file.Name;
+
+                // 入力ファイル
+                var sr = new StreamReader(
+                    file.FullName,
+                    Encoding.GetEncoding("UTF-8"));
+                // 出力ファイル
+                var sw = new StreamWriter(
+                    textOutputFolder.Text + @"\" + file.Name,
+                    false, // 新規作成 or 上書き
+                    Encoding.GetEncoding("UTF-8"));
+
+                // 1行ずつ読み込んで処理
+                while (sr.Peek() > -1)
+                {
+                    string line = sr.ReadLine();
+
+                    // 各々の変数名について
+                    foreach (var varName in varNames)
+                    {
+                        // 括弧の置換 (1行の文字列, 括弧の前の変数名)
+                        line = ConvertLine(line, varName);
+                    }
+                    sw.WriteLine(line);
+                }
+                //ファイルを閉じる
+                sw.Close();
+                sr.Close();
+            }
+            textStatus.Text = "";
+            MessageBox.Show(
+                files.Length.ToString() + "個のファイルを処理しました。\n" + 
+                "エラー件数: " + ErrorNum.ToString());
+        }
+
+        // 括弧の置換 (1行の文字列, 括弧の前の変数名)
+        private string ConvertLine(string line, string varName)
+        {
+            var sbLine = new StringBuilder(line);
+            bool changed = false;
+
+            // 単語検索(正規表現)で変数名を検索
+            MatchCollection mc = Regex.Matches(line, String.Format(@"\b{0}\b", varName));
+
+            // 検索結果すべてについて
+            foreach (Match m in mc)
+            {
+                // 変数の直後の位置
+                int pos = m.Index + varName.Length;
+
+                // 変数の直後に ( があるか探して置換。
+                int bracketPos = findOpeningBracket(sbLine, pos);
+                if (bracketPos > 0)
+                {
+                    // 対応する ) を探して置換。
+                    findClosingBracket(sbLine, pos);
+                    changed = true;
+                }
+            }
+
+            if (changed) {
+                line = sbLine.ToString();
+            }
+            return line;
+        }
+
+        // 変数の直後に ( があるか探して置換 
+        // sbLine: 1行の文字列
+        // pos: 変数の直後の位置
+        // return: ( の直後の位置　(見つからなかったら 0 を返す)
+        private int findOpeningBracket(StringBuilder sbLine, int pos)
+        {
+            for (int i = pos; i < sbLine.Length; i++)
+            {
+                char c = sbLine[i];
+                if (c == ' ' || c == '\t')
+                {
+                    // 空白は飛ばして次の文字へ
+                }
+                else if (c == '(')
+                {
+                    // ( があれば [ に置換して次の処理へ。位置は ( の直後から
+                    sbLine[i] = '[';
+                    return i + 1;
+                }
+                else
+                {
+                    // 変数名の次に ( が無い場合
+                    return 0;
+                }
+            }
+            // 行末まで空白しか無い場合
+            return 0;
+        }
+
+        // 対応する ) を探して置換
+        // sbLine: 1行の文字列
+        // pos: ( の直後の位置
+        private void findClosingBracket(StringBuilder sbLine, int pos)
+        {
+            int nest = 1;
+            for (int i = pos; i < sbLine.Length; i++)
+            {
+                char c = sbLine[i];
+                if (c == '(')
+                {
+                    // ( があればネスト段数をカウントアップ
+                    nest++;
+                }
+                else if (c == ')')
+                {
+                    // ) があればネスト段数をカウントダウン
+                    nest--;
+                    if (nest == 0)
+                    {
+                        // 対応する ) があれば ] に置換して
+                        // 変数の検索を再開
+                        sbLine[i] = ']';
+                        return;
+                    }
+                }
+            }
+            // 対応する ) が無い場合 (エラー)
+            ErrorNum++;
+            sbLine.Append(" <ERROR> 1行の中で閉じない括弧");
+            return;
+        }
+    }
+}
